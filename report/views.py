8acdc4e7c09faaf46037147a81b4b969b6759550
Rodrigo de Lima Vieira <rodrigodelimavieira@gmail.com>
2016-03-17 13:46:18 -0300

    Basic incidents list
    Testing bootstrap with basic incidents list view and template.
    Using django template extension.

--- report/views.py
+++ report/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Incident
 
-# Create your views here.
+
+def report_list(request):
+    incidents = Incident.objects.order_by('-date_time')
+    return render(request, 'report/report_list.html', {'incidents': incidents})
